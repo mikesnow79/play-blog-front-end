3649bc34461a9fa2fc80a6c35c1ec64529838b26
Mike Snow <mikesnow79@gmail.com>
2018-01-11 10:46:31 -0500

    add inline-source-map

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import './tufte.css';
 
-console.log('Jellow World from front-end repo');
+cosnole.log('Jellow World from front-end repo');
 function component() {
   let element = document.createElement('div');
   element.innerHTML = '<article><h1>Hello World</h1><p class="subtitle">Hello Webpack</p></article>';
