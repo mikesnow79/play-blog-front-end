89afdd978df262c8cb813f07379ad7c27a4d0ee4
Mike Snow <mikesnow79@gmail.com>
2018-01-11 07:24:48 -0500

    Refactor fonts loader in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,18 +17,23 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+            useRelativePath: true
+          }
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ],
-        // options: {
-        //   name: 'fonts/[name].[ext]'
-        // }
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+            // useRelativePath: true
+          }
+        }
       }
     ]
   }
