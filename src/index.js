88ab3a631979bf03eee675083b311ecf029216cf
Mike Snow <mikesnow79@gmail.com>
2018-01-11 10:21:47 -0500

    Add app main container

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ console.log('Jellow World from front-end repo');
 function component() {
   let element = document.createElement('div');
   element.innerHTML = '<article><h1>Hello World</h1><p class="subtitle">Hello Webpack</p></article>';
+  element.setAttribute('id', 'main-container');
   return element;
 }
 
