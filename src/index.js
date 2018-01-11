90abc33249cd7ee5a6c48b3bca0a63b6c88166d2
Mike Snow <mikesnow79@gmail.com>
2018-01-11 15:47:31 -0500

    Add paragraph content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './tufte.css';
 console.log('Jellow World from front-end repo');
 function component() {
   let element = document.createElement('div');
-  element.innerHTML = '<article><h1>Hello World</h1><p class="subtitle">Hello Webpack</p></article>';
+  element.innerHTML = '<article><h1>Mike Snow</h1><p class="subtitle">His website</p><section><p>Tufte CSS provides tools to style web articles using the ideas demonstrated by Edward Tufte’s books and handouts. Tufte’s style is known for its simplicity, extensive use of sidenotes, tight integration of graphics with text, and carefully chosen typography.</p></section></article>';
   element.setAttribute('id', 'main-container');
   return element;
 }
