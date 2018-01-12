78e6b86189e24b1bc868bdbee3a6833ed6ca9f49
Mike Snow <mikesnow79@gmail.com>
2018-01-12 17:45:47 -0500

    add jest and get it working correctly

--- webpack.config.js
+++ webpack.config.js
@@ -50,9 +50,6 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
         }
       }
     ]
