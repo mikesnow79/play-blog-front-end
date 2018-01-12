e7cc9441fc1e55b011ee2fbb9b7368b9cb3547a4
Mike Snow <mikesnow79@gmail.com>
2018-01-12 14:15:12 -0500

    add commented react basic app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import path from 'path';
 import './tufte.css';
 import './jim_brown.jpeg';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { AppContainer } from 'react-hot-loader';
+// import Counter from './components/Counter';
 
-console.log('Jellow World from front-end repo');
-const MYNAME = 'mike';
 function component() {
   let element = document.createElement('div');
-  element.innerHTML = '<article><h1>Mike Snow</h1><p class="subtitle">His website</p><section><p>Tufte CSS provides tools to style web articles using the ideas demonstrated by Edward Tufte’s books and handouts. Tufte’s style is known for its simplicity, extensive use of sidenotes, tight integration of graphics with text, and carefully chosen typography.</p><figure><img src="/static/jim_brown.jpeg"></figure></section></article>';
-  element.setAttribute('id', 'main-container');
+  let openParaText = '<section><p>Tufte CSS provides tools to style web articles using the ideas demonstrated by Edward Tufte’s books and handouts. Tufte’s style is known for its simplicity, extensive use of sidenotes, tight integration of graphics with text, and carefully chosen typography.</p><figure><img src="/static/jim_brown.jpeg"></figure></section>';
+  let mainContainerId = 'main-container';
+  element.innerHTML = `<article><h1>Mike Snow</h1><p class="subtitle">His website</p>${openParaText}</article>`;
+  element.setAttribute('id', mainContainerId);
   return element;
 }
 
 document.body.appendChild(component());
+
+// render(
+  {/*<AppContainer>*/}
+  //
+  // </AppContainer>,
+  // document.getElementById('main-container')
+// );
 
