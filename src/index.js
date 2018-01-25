63342001c7044570ca85bea61c76089706ae10bf
Mike Snow <mikesnow79@gmail.com>
2018-01-25 15:10:27 -0500

    Import React and add basic login page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-import './tufte.css';
-import './images/jim_brown.jpeg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {Row, Col} from 'react-flexbox-grid';
+
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
+import Login from './views/Login';
+
 
 /* eslint-disable no-console */
 
 function component() {
   let element = document.createElement('div');
-  let openParaText = '<section><p>Tufte CSS provides tools to style web articles using the ideas demonstrated by Edward Tufte’s books and handouts. Tufte’s style is known for its simplicity, extensive use of sidenotes, tight integration of graphics with text, and carefully chosen typography.</p><figure><img src="/static/jim_brown.jpeg"></figure></section>';
   let mainContainerId = 'main-container';
-  element.innerHTML = `<article><h1>Mike Snow</h1><p class="subtitle">His website</p>${openParaText}</article>`;
   element.setAttribute('id', mainContainerId);
   return element;
 }
 
 document.body.appendChild(component());
 
-// render(
-//   <Counter />,
-//   document.getElementById('main-container')
-// );
 
+ReactDOM.render(<MuiThemeProvider>
+  <Router>
+    <Row around="xs">
+      <Col xs={12} md={11}>
+          <Route exact path="/index" component={Login} />
+        {/*<Route exact path="/app" component={CartoonsDirectory} />*/}
+      </Col>
+    </Row>
+  </Router>
+</MuiThemeProvider>, document.getElementById('main-container'));
